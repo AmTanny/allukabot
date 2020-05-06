4cb496c34d110ac22d6f5fa35362124ee64e2816
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-05-06 10:53:19 +0530

    Update __init__.py

--- alluka/__init__.py
+++ alluka/__init__.py
@@ -173,7 +173,8 @@ if ENV:
     WALL_API = os.environ.get('WALL_API',None)
     LYDIA_API = os.environ.get('LYDIA_API',None)
     DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN',None)
-    sw = os.environ.get('SW_API',None)
+    spamwatch_api = os.environ.get('SW_API',None)
+    sw = spamwatch.Client(spamwatch_api)
 
 
 
