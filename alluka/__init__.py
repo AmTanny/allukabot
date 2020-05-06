8c29d1f3cee09dd0eb7d20d00eaffd3410246592
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-05-06 10:15:08 +0530

    Update __init__.py

--- alluka/__init__.py
+++ alluka/__init__.py
@@ -22,6 +22,84 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     )
     quit(1)
 
+ENV = bool(os.environ.get('ENV', True))
+# Load config
+try:
+    CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
+except FileNotFoundError:
+    print("Read README.md file again. you missed something.")
+    quit(1)
+except Exception as eee:
+    print(
+        f"Ah, look like there's error(s) while trying to load your config. It is\n!!!! ERROR BELOW !!!!\n {eee} \n !!! ERROR END !!!"
+    )
+    quit(1)
+
+if not CONFIG['alluka_explain_config'] == "alluka_zoldyck":
+    print("Read config.yml file carefully.")
+    quit(1)
+TOKEN = CONFIG['bot_token']
+API_KEY = CONFIG['api_key']
+API_HASH = CONFIG['api_hash']
+
+
+try:
+    OWNER_ID = int(CONFIG['owner_id'])
+
+except ValueError:
+    raise Exception("Your 'owner_id' variable is not a valid integer.")
+
+try:
+    MESSAGE_DUMP = CONFIG['message_dump']
+except ValueError:
+    raise Exception("Your 'message_dump' must be set.")
+
+try:
+    GBAN_LOGS = CONFIG['gban_dump']
+except ValueError:
+    raise Exception("Your 'gban_dump' must be set.")
+
+try:
+    OWNER_USERNAME = CONFIG['owner_username']
+except ValueError:
+    raise Exception("Your 'owner_username' must be set.")
+try:
+    SUDO_USERS = set(int(x) for x in CONFIG['sudo_users'] or [])
+except ValueError:
+    raise Exception("Your sudo users list does not contain valid integers.")
+try:
+    SUPPORT_USERS = set(int(x) for x in CONFIG['support_users'] or [])
+except ValueError:
+    raise Exception("Your support users list does not contain valid integers.")
+try:
+    DEV_USERS = set(int(x) for x in CONFIG['dev_users'] or [])
+except ValueError:
+    raise Exception("Your dev users list does not contain valid integers.")
+try:
+    WHITELIST_USERS = set(int(x) for x in CONFIG['whitelist_users'] or [])
+except ValueError:
+    raise Exception(
+        "Your whitelisted users list does not contain valid integers.")
+
+DB_URI = CONFIG['database_url']
+LOAD = CONFIG['load']
+NO_LOAD = CONFIG['no_load']
+DEL_CMDS = CONFIG['del_cmds']
+WORKERS = CONFIG['workers']
+ALLOW_EXCL = CONFIG['allow_excl']
+SPAMMERS = CONFIG['spammers_lists']
+CERT_PATH = CONFIG['cert_path']
+PORT = CONFIG['port']
+CASH_API_KEY = CONFIG['cash_api']
+LYDIA_API = CONFIG['lydia_api']
+TIME_API_KEY = CONFIG['time_api']
+WALL_API = CONFIG['wall_api']
+STRICT_GBAN = CONFIG['strict_gban']
+STRICT_GMUTE = CONFIG['strict_gmute']
+WEBHOOK = CONFIG['webhook']
+URL = CONFIG['url']
+BAN_STICKER = CONFIG['ban_sticker']
+
 ENV = bool(os.environ.get('ENV', False))
 
 if ENV:
@@ -96,85 +174,6 @@ if ENV:
     LYDIA_API = os.environ.get('LYDIA_API',None)
     DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN',None)
     spamwatch_api = os.environ.get('SW_API',None)
-
-# Load config
-try:
-    CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
-except FileNotFoundError:
-    print("Read README.md file again. you missed something.")
-    quit(1)
-except Exception as eee:
-    print(
-        f"Ah, look like there's error(s) while trying to load your config. It is\n!!!! ERROR BELOW !!!!\n {eee} \n !!! ERROR END !!!"
-    )
-    quit(1)
-
-if not CONFIG['alluka_explain_config'] == "alluka_zoldyck":
-    print("Read config.yml file carefully.")
-    quit(1)
-
-TOKEN = CONFIG['bot_token']
-API_KEY = CONFIG['api_key']
-API_HASH = CONFIG['api_hash']
-
-
-try:
-    OWNER_ID = int(CONFIG['owner_id'])
-
-except ValueError:
-    raise Exception("Your 'owner_id' variable is not a valid integer.")
-
-try:
-    MESSAGE_DUMP = CONFIG['message_dump']
-except ValueError:
-    raise Exception("Your 'message_dump' must be set.")
-
-try:
-    GBAN_LOGS = CONFIG['gban_dump']
-except ValueError:
-    raise Exception("Your 'gban_dump' must be set.")
-
-try:
-    OWNER_USERNAME = CONFIG['owner_username']
-except ValueError:
-    raise Exception("Your 'owner_username' must be set.")
-try:
-    SUDO_USERS = set(int(x) for x in CONFIG['sudo_users'] or [])
-except ValueError:
-    raise Exception("Your sudo users list does not contain valid integers.")
-try:
-    SUPPORT_USERS = set(int(x) for x in CONFIG['support_users'] or [])
-except ValueError:
-    raise Exception("Your support users list does not contain valid integers.")
-try:
-    DEV_USERS = set(int(x) for x in CONFIG['dev_users'] or [])
-except ValueError:
-    raise Exception("Your dev users list does not contain valid integers.")
-try:
-    WHITELIST_USERS = set(int(x) for x in CONFIG['whitelist_users'] or [])
-except ValueError:
-    raise Exception(
-        "Your whitelisted users list does not contain valid integers.")
-
-DB_URI = CONFIG['database_url']
-LOAD = CONFIG['load']
-NO_LOAD = CONFIG['no_load']
-DEL_CMDS = CONFIG['del_cmds']
-WORKERS = CONFIG['workers']
-ALLOW_EXCL = CONFIG['allow_excl']
-SPAMMERS = CONFIG['spammers_lists']
-CERT_PATH = CONFIG['cert_path']
-PORT = CONFIG['port']
-CASH_API_KEY = CONFIG['cash_api']
-LYDIA_API = CONFIG['lydia_api']
-TIME_API_KEY = CONFIG['time_api']
-WALL_API = CONFIG['wall_api']
-STRICT_GBAN = CONFIG['strict_gban']
-STRICT_GMUTE = CONFIG['strict_gmute']
-WEBHOOK = CONFIG['webhook']
-URL = CONFIG['url']
-BAN_STICKER = CONFIG['ban_sticker']
-
 
 
 
