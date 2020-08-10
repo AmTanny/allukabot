aa82a0b87387621737ed37e95e6204b8c2873200
Tanny <62448159+AmTanny@users.noreply.github.com>
2020-08-10 12:05:21 -0700

    Update truth_and_dare.py

--- alluka/modules/truth_and_dare.py
+++ alluka/modules/truth_and_dare.py
@@ -27,5 +27,5 @@ DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 
-__mod_name__ = "truth"
+__mod_name__ = "Truth or Dare"
 
